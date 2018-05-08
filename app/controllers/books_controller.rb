62f93bd776a7ee117e8fd3d46714fe08c70f6958
Linda Ogbevoen <lindaogbevoen@gmail.com>
2018-05-08 02:26:16 +0000

    added the simple form gem

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,7 +4,25 @@ class BooksController < ApplicationController
     @books = Book.all
   end
 
+  def new
+    @book = Book.new
+    
+  end
+
+  def create
+    @book = Book.create(book_params)
+    redirect_to root_path
+
+  end
+
   def about
+  end
+
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :author, :summary)
   end
 
 end
